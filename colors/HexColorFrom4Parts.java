cad0896a1aa13d4040cdb43773ba1b1f0cbbc511
ruan65 <ruan65@gmail.com>
2015-02-07 12:04:29 +0300

    correction

--- colors/HexColorFrom4Parts.java
+++ colors/HexColorFrom4Parts.java
@@ -1,9 +1,14 @@
 public class HexColorFrom4parts {
 
 	public static int composeHex(int alpha, int r, int g, int b) {
-		String hexString = Integer.toHexString(alpha) + Integer.toHexString(r)
-				+ Integer.toHexString(g) + Integer.toHexString(b);
+
+		String hexString = toHex(alpha) + toHex(r) + toHex(g) + toHex(b);
+
 		return (int) Long.parseLong(hexString, 16);
 	}
 
+    private static String toHex(int n) {
+    
+        return n > 15 ? Integer.toHexString(n) : "0" + Integer.toHexString(n);
+    }
 }
