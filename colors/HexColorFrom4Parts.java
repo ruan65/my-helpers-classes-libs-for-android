ddef22913d1ac46ebe7d872513a851ebf73475b3
ruan65 <ruan65@gmail.com>
2015-03-25 01:55:57 +0300

    Hex color string to int argb array converter added

--- colors/HexColorFrom4Parts.java
+++ colors/HexColorFrom4Parts.java
@@ -8,7 +8,36 @@ public class HexColorFrom4parts {
 	}
 
     private static String toHex(int n) {
-    
+
         return n > 15 ? Integer.toHexString(n) : "0" + Integer.toHexString(n);
+    }
+
+    /**
+     * Hex color string to int[] array converter
+     *
+     * @param hexARGB should be color hex string: #AARRGGBB or #RRGGBB
+     * @return int[] array: [alpha, r, g, b]
+     * @throws IllegalArgumentException
+     */
+
+    public static int[] hexStringToARGB(String hexARGB) throws IllegalArgumentException {
+
+        Log.d("ml", "hexARGB = " + hexARGB);
+
+        if (!hexARGB.startsWith("#") || !(hexARGB.length() == 7 || hexARGB.length() == 9)) {
+
+            throw new IllegalArgumentException("Hex color string is incorrect!");
+        }
+
+        int[] intARGB = new int[4];
+
+        if (hexARGB.length() == 9) {
+            intARGB[0] = Integer.valueOf(hexARGB.substring(1, 3), 16); // alpha
+            intARGB[1] = Integer.valueOf(hexARGB.substring(3, 5), 16); // red
+            intARGB[2] = Integer.valueOf(hexARGB.substring(5, 7), 16); // green
+            intARGB[3] = Integer.valueOf(hexARGB.substring(7), 16); // blue
+        } else hexStringToARGB("#FF" + hexARGB.substring(1));
+
+        return intARGB;
     }
 }
